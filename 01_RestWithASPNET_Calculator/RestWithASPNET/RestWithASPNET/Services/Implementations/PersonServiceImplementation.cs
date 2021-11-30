88ceedcf709d9e6918772390fe6ae2aacdbeb855
Marcelo Bastos <marcelobastoscarneiro.dev@gmail.com>
2021-11-30 18:34:50 -0300

    commit alterações

---
+++ 01_RestWithASPNET_Calculator/RestWithASPNET/RestWithASPNET/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,71 @@
+﻿using RestWithASPNET.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        private Person person;
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+            for (int i = 0; i < 8; i++)
+            {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+            return persons;
+        }
+
+
+        public Person FindById(long id)
+        {
+            return new Person 
+            { 
+                Id = IncrementAndGet(),
+                FirstName = "Marcelo",
+                LastName = "Carneiro",
+                Address = "Fortaleza - Ceará - Brasil",
+                Gender = "Male"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Person Name" + i,
+                LastName = "Person Last Name" + i,
+                Address = "Some Address" + i,
+                Gender = "Male"
+            };
+        }
+
+        private int IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+    }
+}
